91a005cac74a145fadbbde7b35540582c7c3284f
William Watson <williamdwatson1@gmail.com>
2023-12-18 13:45:36 -0600

    Add build branch

--- settings.py
+++
@@ -1,6 +0,0 @@
-#: Path to where data files are stored
-FILE_DIR = ''
-#: Number of embedding dimensions for each move
-EMBEDDING_DIM = 72
-#: Maximum number of moves in a game
-SEQUENCE_LENGTH = 150
